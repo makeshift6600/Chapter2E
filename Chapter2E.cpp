d36ff4b04971a8d6d8ec5f941e7c862078d20ac1
makeshift6600 <j.l.cannady04@gmail.com>
2025-09-17 08:16:54 -0400

    First attempt

--- Chapter2E.cpp
+++ Chapter2E.cpp
@@ -1,11 +1,21 @@
 /*
-
-
+File name: Chapter2E.cpp
+Date: 9/2025
+Programmer name: Jamen Cannady
+Requirements: Takes two numbers and adds and displays the sum.
 */
+
 #include <iostream>
+
+using namespace std;
+const int number1 = 100;
+const int number2 = 200;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int sum = number1 + number2;
+
+	cout << "The sum of " << number1 << " and " << number2 << " is " << sum;
 }
 
